77b02ca156d33cab1e8f41fd9b862d204628c493
Suzanne <suzanne.chung@mohawkcollege.ca>
2025-02-12 00:43:51 -0500

    PowerUp Creation and Effect

---
+++ DesignWeekWinter2025/Assets/Scripts/HealthPowerUp.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPowerUp : MonoBehaviour
+{
+    //Update health boost value as needed
+    public int healthboost = 10;
+
+    void OnTriggerEnter (Collider other)
+    {
+        if (other.CompareTag("Peasant"))
+        {
+            Pickup(other);
+        }
+    }
+
+    void Pickup(Collider player)
+    {
+        player.GetComponent<Player1Controller>().health += healthboost;
+        player.GetComponent<Player2Controller>().health += healthboost;
+        Destroy(gameObject);
+    }
+}
---
+++ DesignWeekWinter2025/Assets/Scripts/Wolfsbane.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wolfbane : MonoBehaviour
+{
+    //Update slowdown speed value as needed
+    public float slowdown = 5f;
+    
+    //Update tag once werewolf is declared
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Werewolf"))
+        {
+            Pickup(other);
+        }
+    }
+
+    void Pickup(Collider player)
+    {
+        player.GetComponent<WerewolfController>().dashSpeed -= slowdown;
+        Destroy(gameObject);
+    }
+}
